9db89124edec670a8445aa382750f1e040405c67
tushar <tyagi121201@gmail.com>
2021-07-26 23:51:21 +0530

    message

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -13,8 +13,8 @@ class WeatherList extends Component {
 
     return (
       <tr key={name}>
-        <td><GoogleMap lon={lon} lat={lat} /></td>
-        {/* <td>{name}</td> */}
+        {/* <td><GoogleMap lon={lon} lat={lat} /></td> */}
+        <td className="name">{name}</td>
         <td><Chart data={temps} color="orange" units="K" /></td>
         <td><Chart data={pressures} color="green" units="hPa" /></td>
         <td><Chart data={humidities} color="black" units="%" /></td>
